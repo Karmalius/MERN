1a1b6f4db73e1cd7fc0a6ecc718f659b730c572d
Misumuru <55229180+Misumuru@users.noreply.github.com>
2021-02-05 11:28:57 +0200

    ??

---
+++ WS2/combiningfiles.js
@@ -0,0 +1,25 @@
+// Asynkronisesti luodaan tiedosto ja lisätään sinne toisista tiedostoista luetut tekstit
+
+var fs = require("fs");
+
+fs.writeFile('uusiFile.txt', "Tässä uuden tiedoston sisältö");
+
+fs.readFile('example.txt', 
+  function (err, data) {
+    if (err) {
+      console.log('Tapahtui virhe!')
+  }
+  var data = data.toString();
+})
+
+fs.readFile('example2.txt', 
+  function (err, secondData) {
+    if (err) {
+      console.log('Tapahtui virhe!')
+  }
+  var secondData = secondData.toString();
+})
+
+fs.appendFile('uusiFile.txt' + data + secondData);
+
+// ????
--- WS2/readingfiles.js
+++ WS2/readingfiles.js
@@ -1,1 +1,27 @@
-console.log("start");
+// Synkronisesti
+//var data = fs.readFileSync("example.txt");
+//var secondData = fs.readFileSync("example2.txt");
+
+//console.log("Luettu tiedostoista:");
+//console.log(data.toString() + " ja " + secondData.toString());
+
+// Ja asynkronisesti
+var fs = require('fs')
+
+var data = fs.readFile('example.txt', 
+  function (err, data) {
+    if (err) {
+      console.log('Tapahtui virhe!')
+  }
+  console.log('Luettu tiedosto:')
+  console.log(data.toString())
+})
+
+var secondData = fs.readFile('example2.txt', 
+
+  function (err, secondData) {
+    if (err) {
+      console.log('Tapahtui virhe!')
+  }
+  console.log(secondData.toString())
+})
