7bdf50d4b272fa8b6ccdebdf519bb7748e6a976f
Misumuru <55229180+Misumuru@users.noreply.github.com>
2021-02-06 12:45:05 +0200

    Completed

--- WS2/routeswithactions.js
+++ WS2/routeswithactions.js
@@ -1,32 +1,39 @@
 var http = require("http");
-var fs = require("fs");
+var fs = require('fs')
 
 http.createServer(function (request, response) {
-    
-    //if (request.url === "/") {
-        //response.writeHead(200, { "Content-Type": "text/plain" });
-        //response.write("Nothing here to see");
-        response.writeHead(200, { "Content-Type": "text/html" });
-    //if (request.url === "/frontpage") {
-        
-        fs.readFile('./index.html', null, function (error, data) {
-            if (error) {
-                response.writeHead(404);
-                respone.write('Whoops! File not found!');
-            } else {
-                response.write(data);
-            }
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    if (request.url === "/") {
+            response.write("Nothing here to see");
+            response.end();
+    }
+    else if (request.url === "/frontpage") {
+        fs.readFile('index.html', function (err, data) {
+            response.writeHead(200, { "Content-Type": "text/html" });
+
+            response.write(data);
             response.end();
         });
-    //} else if (request.url === "/plaintext") {
-        //response.writeHead(200, { "Content-Type": "text/plain" });
-        //response.write("Here comes plain text");
-    //} else if (request.url === "/json") {
-        //response.writeHead(200, { "Content-Type": "text/plain" });
-        //response.write("Here comes some json");
-    //}
+    }
+    else if (request.url === "/contact") {
+        fs.readFile("contact.html", function (err, data) {
+            response.writeHead(200, { "Content-Type": "text/html" });
 
+            response.write(data);
+            response.end();
+        });
+    } else if (request.url === "/plaintext") {
+        fs.readFile('example.txt', function (err, data) {
+            response.writeHead(200, { "Content-Type": "text/plain" });
 
-    response.end();
+            response.write(data);
+            response.end();
+        });
+    } else if (request.url === "/json") {
+        var data = require("./sampledata.json");
+        var output = JSON.stringify(data);
+        response.write(output);
+        response.end();
+    }
   })
   .listen(3000);
