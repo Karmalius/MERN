d54859964925c906d0c13dd88de4ee8b20208b87
Misumuru <55229180+Misumuru@users.noreply.github.com>
2021-02-17 12:03:29 +0200

    Update movies.js

--- WS3/movies.js
+++ WS3/movies.js
@@ -2,31 +2,28 @@ const request = require("request");
 var http = require("http");
 var data;
 
-request("http://www.omdbapi.com/?i=tt3896198&apikey=3d3574c1", 
+request("http://www.omdbapi.com/?t=harry+potter&apikey=3d3574c1", 
 {json: true}, 
 (err, res, body) => {
     if (err) {
         return console.log(err); 
     }
     data = body;
+    console.log(res);
     console.log(body);
 });
-
-function parse(data) {
-    var html = "<table>";
-    html += "<tr><td>" + data.title + "</td></tr>";
-    html += "</table>";
-    console.log(html);
-    return html;
-}
 
 http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/html" });
 
-    response.write("<table>");
-    response.write("<tr><td>" + data.title + "</td></tr>");
+    response.write("<table border=1>");
+    response.write("<tr><td>Name: " + data.Title + "</td></tr>");
+    response.write("<tr><td>Year: " + data.Year + "</td></tr>");
+    response.write("<tr><td>Genre: " + data.Genre + "</td></tr>");
+    response.write("<tr><td>Plot: " + data.Plot + "</td></tr>");
+    response.write("<tr><td>Rating: (" + data.Ratings[0].Source + ") " + data.Ratings[0].Value + "</td></tr>");
     response.write("</table>");
 
-    response.end("Toimii");
+    response.end();
   })
   .listen(8081);
