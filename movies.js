cf28bb12429715a7c5c9cb80b8400014cebea48d
Misumuru <55229180+Misumuru@users.noreply.github.com>
2021-02-17 12:39:46 +0200

    edit

---
+++ movies.js
@@ -0,0 +1,29 @@
+const request = require("request");
+var http = require("http");
+var data;
+
+request("http://www.omdbapi.com/?t=harry+potter&apikey=3d3574c1", 
+{json: true}, 
+(err, res, body) => {
+    if (err) {
+        return console.log(err); 
+    }
+    data = body;
+    console.log(res);
+    console.log(body);
+});
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+
+    response.write("<table border='1'>");
+    response.write("<tr><td>Name: " + data.Title + "</td></tr>");
+    response.write("<tr><td>Year: " + data.Year + "</td></tr>");
+    response.write("<tr><td>Genre: " + data.Genre + "</td></tr>");
+    response.write("<tr><td>Plot: " + data.Plot + "</td></tr>");
+    response.write("<tr><td>Rating: (" + data.Ratings[0].Source + ") " + data.Ratings[0].Value + "</td></tr>");
+    response.write("</table>");
+
+    response.end();
+  })
+  .listen(8081);
