96761c6f97e8128909f10e530cbe3545317f1fe4
Karmalius <55229180+Karmalius@users.noreply.github.com>
2021-03-04 11:53:52 +0200

    Valmis

--- WS5/server.js
+++ WS5/server.js
@@ -11,9 +11,6 @@ app.set("view engine", "ejs");
 // Tällä pakotetaan sivupohja tuottamaan sisennettyä, kaunista HTML:ää. 
 app.locals.pretty = true;
 
-// Sisätlö haetaan public-hakemistosta.
-//app.use(express.static("./views"));
-
 // Luodaan sivu. Res.render()-funktio lähettää sivut template-moottorille ennen niiden päätymistä loppukäyttäjälle selaimeen.
 app.get("/", function(req, res) {
     res.render("pages/index", {
